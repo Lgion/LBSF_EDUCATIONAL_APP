95d5b236561dbc62fba8e73c423f20680821bf9d
athena <legion.athenienne@gmail.com>
2025-02-28 11:23:24 +0000

    COMPLËTE CASCADE SOLUTION COMMIT

--- READMEs/website/astro.config.mjs
+++ READMEs/website/astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     react(),
     tailwind({
